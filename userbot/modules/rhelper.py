e5ab44fe09b30f6857e554b73e2c76072e68ed85
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-01-29 05:58:49 +0700

    Update rhelper.py

--- userbot/modules/rhelper.py
+++ userbot/modules/rhelper.py
@@ -12,9 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
-        "\n[LANDAK🦔](t.me/teman_random)"
+        "\n[LANDAK🦔](t.me/trvgroup)"
         "\n\n[SUPPORT](https://t.me/geezsupportgroup)"
-        "\n\n[CHANNEL](https://t.me/ramubotinfo)")
+        "\n\n[CHANNEL](https://t.me/stryvrtu)")
 
 
 @register(outgoing=True, pattern="^.rvars$")
