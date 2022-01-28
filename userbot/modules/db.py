256e553dd582fbc2241728dbd48aad6e637be0d9
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-01-29 00:59:01 +0700

    Update db.py

--- userbot/modules/db.py
+++ userbot/modules/db.py
@@ -8,7 +8,7 @@
 from platform import uname
 
 
-from userbot import ALIVE_NAME, BOT_VER, is_mongo_alive, is_redis_alive
+from userbot import ALIVE_NAME, BOT_VER, bot
 from userbot.events import register
 
 # ================= CONSTANT =================
