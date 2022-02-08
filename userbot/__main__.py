1c61034164a4097dc7c81a38ba0caba13e2a3efa
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-08 16:36:32 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -52,7 +52,12 @@ async def ram_userbot_on():
     except BaseException:
         pass
 
+bot.loop.run_until_complete(waiting())
+bot.loop.run_until_complete(checking())
 bot.loop.run_until_complete(ram_userbot_on())
+if not BOT_TOKEN:
+    bot.loop.run_until_complete(autobot())
+idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
