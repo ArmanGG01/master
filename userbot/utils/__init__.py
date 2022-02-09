2ceb3885c12926e6c4c56a8ee95296a50a8f1f26
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-09 19:53:40 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -13,6 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from . import format as _format
 from .format import parse_pre
 from .chrome import chrome, options
 #from .decorator import rose_cmd, rose_handler
@@ -28,4 +29,5 @@ from .tools import (
     runcmd,
     edit_delete,
     edit_or_reply,
+    media_type,
 )
