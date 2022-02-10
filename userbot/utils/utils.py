478be9103f5eaddfd7f4296e1f9746aa2c124ce0
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-10 12:07:40 +0700

    Create utils.py

---
+++ userbot/utils/utils.py
@@ -0,0 +1,31 @@
+import importlib
+import sys
+from pathlib import Path
+
+
+from userbot import (
+    LOGS,
+    bot,
+)
+
+
+def start_assistant(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/modules/assistant/{shortname}.py")
+        name = "userbot.modules.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Starting Your Assistant Bot.")
+        LOGS.info("Assistant Sucessfully imported " + shortname)
+    else:
+        path = Path(f"userbot/modules/assistant/{shortname}.py")
+        name = "userbot.modules.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.tgbot = bot.tgbot
+        spec.loader.exec_module(mod)
+        sys.modules["userbot.modules.assistant" + shortname] = mod
+        LOGS.info("Assistant Successfully imported" + shortname)
