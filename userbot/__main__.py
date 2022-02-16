60189d4b8e643a8e12b935aa3b6d747cab33394a
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-17 03:48:15 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -40,7 +40,7 @@ async def ram_ubot_on():
 
 bot.loop.run_until_complete(ram_ubot_on())
 bot.loop.run_until_complete(hadeh_ajg())
-bot.loop.run_untill_complete(autobot())
+bot.loop.run_until_complete(autobot())
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
