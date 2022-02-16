0ffed35b4241520c037071f8ea978923f69180ee
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-17 05:27:49 +0700

    hehehe

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,23 +1,4 @@
-# Copyright (C) 2020 TeamDerUntergang.
-#
-# SedenUserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# Rama ganteng
-# SedenUserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# @Qulec tarafından yazılmıştır.
-# Thanks @Spechide.
-
 import logging
-
 
 from userbot import BOT_USERNAME
 from userbot.events import register
@@ -27,7 +8,7 @@ logging.basicConfig(
     level=logging.WARNING)
 
 
-@register(outgoing=True, pattern=r"^\.rhelp")
+@register(outgoing=True, pattern=r"^\.helpme")
 async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
@@ -39,9 +20,9 @@ async def yardim(event):
             await event.delete()
         else:
             await event.edit(
-                "`Bot tidak berfungsi! Harap setel Token Bot dan Nama Pengguna dengan benar. Modul telah dihentikan.`"
+                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`"
             )
     except Exception:
         return await event.edit(
-            "`Anda tidak dapat Menggunakan Perintah .rhelp, Bisa Jadi Grup Ini tidak Mensupport nya (Mungkin Anda Belum Menghidupakan Inline Mode Dan Inline Location Data)`"
+            "`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)\ngunakan perintah .inlineon`"
         )
