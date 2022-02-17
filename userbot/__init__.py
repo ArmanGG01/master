6bf7a141657cce99f7f78adf2d3739fc0fddf796
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-18 02:50:58 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -527,8 +527,7 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith(
-                    "@Ramubot"):
+            if event.query.user_id == uid and query.startswith("@Ramubot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=geezlogo,
