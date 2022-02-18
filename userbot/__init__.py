49288a235cc0e66a6bb0326cee31e97e9054a3f5
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-19 02:49:26 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -436,7 +436,7 @@ except AttributeError:
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
-    number_of_cols = 4
+    number_of_cols = 2
     global looters
     looters = page_number
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
