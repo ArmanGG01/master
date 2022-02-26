790df0307bf3af4fa48e3fcae24088e830b87ab5
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-02-26 23:11:21 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -243,7 +243,7 @@ async def edit_or_reply(
 eor = edit_or_reply
 
 async def hadeh_ajg():
-    ram = str(pybase64.b64decode("R2VlelN1cHBvcnQ="))[2:13]
+    ram = str(pybase64.b64decode("cmFtc3VwcG9ydGdyb3Vw="))[2:13]
     ubot = str(pybase64.b64decode("VEVFUlZJZ3JvdXA="))[2:13]
     try:
         await bot(Get(ram))
