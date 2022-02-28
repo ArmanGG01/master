2e586067ff92a5d07f21c62683f94879e7ebc819
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-01 04:09:17 +0700

    Update ramfaction.py

--- userbot/modules/ramfaction.py
+++ userbot/modules/ramfaction.py
@@ -1,11 +1,12 @@
 # Port By @VckyouuBitch From Geez-Projects
 # # Copyright (C) 2021 Geez-Project
 from userbot.events import register
-from userbot import CMD_HELP
+from userbot import CMD_HELP, DEVS
 import asyncio
 
 
 @register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
+@register(incoming=True, from_users=DEVS, pattern=^.ctyp(?: |$)(.*)")
 async def _(landak):
     t = landak.pattern_match.group(1)
     if not (t or t.isdigit()):
