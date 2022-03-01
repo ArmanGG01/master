17ba656f603660f56aa99fbbd48ffb94371142f8
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-02 06:51:11 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,7 +9,7 @@ import sys
 from importlib import import_module
 
 from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, BOT_VER, LOGS, bot, ramblacklist
+from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, BOT_VER, LOGS, bot, ramblacklist, checking
 from userbot.modules import ALL_MODULES
 from userbot.utils.utils import autobot
 try:
