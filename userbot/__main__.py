6626649bf51d1bfd987583d2706f0d8e8705dfe8
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-02 07:10:33 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,9 +9,9 @@ import sys
 from importlib import import_module
 
 from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, BOT_VER, LOGS, bot, ramblacklist, checking
+from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, BOT_VER, LOGS, bot, ramblacklist
 from userbot.modules import ALL_MODULES
-from userbot.utils.utils import autobot
+from userbot.utils. import autobot, hadeh_ajg
 try:
     for module_name in ALL_MODULES:
         imported_module = import_module("userbot.modules." + module_name)
@@ -43,7 +43,7 @@ async def ram_ubot_on():
         pass
 
 bot.loop.run_until_complete(autobot())
-bot.loop.run_until_complete(checking())
+bot.loop.run_until_complete(hadeh_ajg())
 bot.loop.run_until_complete(ram_ubot_on())
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
