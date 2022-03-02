d60f3b149054bfbd7e029afce19ae625c5fa8daf
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-02 10:55:43 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -269,7 +269,7 @@ OWNER_BOT = os.environ.get(
     "OWNER_BOT") or "t.me/teervigroup"
 
 # Default botlog
-BOTLOG_MSG = os.environ.get("BOTLOG_MSG") or f"```💢 RAM - UBOT DAH AKTIF KONTOL!!! 💢\n\n╼┅━━━━━╍━━━━━┅╾\n❍▹ Bot Of : {ALIVE_NAME}\n❍▹ BotVer : {BOT_VER}\n❍▹``` Branch : @RAM_UBOT\n```Jangan Keluar Dari @ramsupportt\nSupaya Kamu Tahu\nApdetan Terbaru nya\n╼┅━━━━━╍━━━━━┅╾```"
+BOTLOG_MSG = os.environ.get("BOTLOG_MSG") or f"```💢 RAM - UBOT DAH AKTIF KONTOL!!! 💢\n\n╼┅━━━━━╍━━━━━┅╾\n❍▹ Bot Of : {ALIVE_NAME}\n❍▹ BotVer : {BOT_VER}\n❍▹``` Branch : @RAM_UBOT\n\n╼┅━━━━━╍━━━━━┅╾\n\n```JANGAN KELUAR DARI GRUP```@RAMSUPPORTT\n```BIAR TAU INFO, KONTOL!!!!!"
 
 # Last.fm Module
 BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
