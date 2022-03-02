48e5d80141759ffe052bd9396f7be1fcc4852594
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-02 14:58:13 +0700

    eh

--- userbot/modules/sangean.py
+++ userbot/modules/sangean.py
@@ -17,7 +17,7 @@ async def _(event):
         asupannya = [
             asupan
             async for asupan in event.client.iter_messages(
-                "@PemuasBangsaIndonesian", filter=InputMessagesFilterVideo
+                "@GirlHd", filter=InputMessagesFilterVideo
             )
         ]
         aing = await event.client.get_me()
