e0af95663ba90724f9c27bbf86e25241935f1d8e
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-02 08:06:23 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -43,9 +43,9 @@ async def ram_ubot_on():
     except BaseException:
         pass
 
+bot.loop.run_until_complete(hadeh_ajg())
 bot.loop.run_until_complete(autobot())
 bot.loop.run_until_complete(ram_ubot_on())
-bot.loop.run_until_complete(hadeh_ajg())
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
