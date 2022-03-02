cee2dce0df8384bc61c80eb83edff410a72107f2
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-03 02:04:01 +0700

    nganu

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -43,7 +43,7 @@ async def autobot():
     if who.username:
         username = who.username + "_ubot"
     else:
-        username = "Asisstant" + (str(who.id))[5:] + "RamUBot"
+        username = "ram" + (str(who.id))[5:] + "ubot"
     bf = "@BotFather"
     await bot(UnblockRequest(bf))
     await bot.send_message(bf, "/cancel")
