a23481d9fcbbc0287e438c6f4e79fefd3c0b2647
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-06 10:54:38 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -15,7 +15,7 @@ from speedtest import Speedtest
 
 from userbot import CMD_HANDLER as cmd
 from userbot.utils import edit_or_reply, ram_cmd
-from userbot import ALIVE_NAME, CMD_HELP, DEVG, StartTime
+from userbot import ALIVE_NAME, CMD_HELP, BOT_VER, DEVG, StartTime
 from userbot.events import register
 
 gesss = [
