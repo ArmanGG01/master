97ab30f3a86156546e4e9f497504f5b8be92d7da
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-06 12:12:02 +0700

    VERSION

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -262,7 +262,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "RAM-UBOT")
 
 # Bot Version
-BOT_VER = os.environ.get("BOT_VER", "9.1.0")
+BOT_VER = os.environ.get("BOT_VER", "9.2.1")
 
 # Default .alive Username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
