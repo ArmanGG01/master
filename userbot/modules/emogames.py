5d2a11edf0150fbb4a312f042e9d6e7d83fafd38
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-11 13:53:07 +0700

    Update emogames.py

--- userbot/modules/emogames.py
+++ userbot/modules/emogames.py
@@ -1,114 +1,128 @@
-# fix by @heyworld for OUB
-# bug fixed by @d3athwarrior
-
 from telethon.tl.types import InputMediaDice
-from userbot.events import register
+
+from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
+from userbot.utils import ram_cmd
 
 
-@register(outgoing=True, pattern="^.dadu(?: |$)(.*)")
+@ram_cmd(pattern="dice(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice(''))
+    r = await event.reply(file=InputMediaDice(""))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice(''))
+                r = await event.reply(file=InputMediaDice(""))
         except BaseException:
             pass
 
 
-@register(outgoing=True, pattern="^.lt(?: |$)(.*)")
+@ram_cmd(pattern="dart(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🎯'))
+    r = await event.reply(file=InputMediaDice("🎯"))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🎯'))
+                r = await event.reply(file=InputMediaDice("🎯"))
         except BaseException:
             pass
 
 
-@register(outgoing=True, pattern="^.basket(?: |$)(.*)")
+@ram_cmd(pattern="basket(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🏀'))
+    r = await event.reply(file=InputMediaDice("🏀"))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🏀'))
+                r = await event.reply(file=InputMediaDice("🏀"))
         except BaseException:
             pass
 
 
-@register(outgoing=True, pattern="^.bola(?: |$)(.*)")
+@ram_cmd(pattern="bowling(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('⚽'))
+    r = await event.reply(file=InputMediaDice("🎳"))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('⚽'))
+                r = await event.reply(file=InputMediaDice("🎳"))
         except BaseException:
             pass
 
 
-@register(outgoing=True, pattern="^.gabruk(?: |$)(.*)")
+@ram_cmd(pattern="ball(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🎳'))
+    r = await event.reply(file=InputMediaDice("⚽"))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🎳'))
+                r = await event.reply(file=InputMediaDice("⚽"))
         except BaseException:
             pass
 
 
-@register(outgoing=True, pattern="^.hoki(?: |$)(.*)")
+@ram_cmd(pattern="jackpot(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🎰'))
+    r = await event.reply(file=InputMediaDice("🎰"))
     if input_str:
         try:
             required_number = int(input_str)
-            while not r.media.value == required_number:
+            while r.media.value != required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🎰'))
+                r = await event.reply(file=InputMediaDice("🎰"))
         except BaseException:
             pass
 
-CMD_HELP.update({
-    "emogames":
-    "`.dadu` 1-6 or `.lt`1-6 or `.bola`1-5 or `.basket`1-5 or `.gabruk`1-5 or `.hoki`1-5\
-\nUsage: Itu Hanya Game emoji yang bisa kalian mainkan beramai ramai`"
-})
+
+CMD_HELP.update(
+    {
+        "emojigames": f"**Plugin : **`emojigames`\
+        \n\n  •  **Syntax :** `{cmd}dice` 1-6\
+        \n  •  **Function : **Memainkan emoji game dice dengan score yg di tentukan kita.\
+        \n\n  •  **Syntax :** `{cmd}dart` 1-6\
+        \n  •  **Function : **Memainkan emoji game dart dengan score yg di tentukan kita.\
+        \n\n  •  **Syntax :** `{cmd}basket` 1-5\
+        \n  •  **Function : **Memainkan emoji game basket dengan score yg di tentukan kita.\
+        \n\n  •  **Syntax :** `{cmd}bowling` 1-6\
+        \n  •  **Function : **Memainkan emoji game bowling dengan score yg di tentukan kita.\
+        \n\n  •  **Syntax :** `{cmd}ball` 1-5\
+        \n  •  **Function : **Memainkan emoji game ball telegram score yg di tentukan kita.\
+        \n\n  •  **Syntax :** `{cmd}jackpot` 1\
+        \n  •  **Function : **Memainkan emoji game jackpot dengan score yg di tentukan kita.\
+        \n\n  •  **NOTE: **Jangan gunakan nilai lebih atau bot akan Crash**\
+    "
+    }
+)
