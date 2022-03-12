f432bcc2e9f7857f77e40cdadd22d59ac95c5bf5
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-12 18:53:46 +0700

    he eleh

--- userbot/modules/roas.py
+++ userbot/modules/roas.py
@@ -14,16 +14,16 @@ from userbot.events import register
 
 @ram_cmd(pattern="roas1(?: |$)(.*)")
 @register(pattern=r"^\.roas(?: |$)(.*)", sudo=True)
-async def _(roaspl):
-    if roaspl.chat_id in BLACKLIST_CHAT:
+async def _(event):
+    if event.chat_id in BLACKLIST_CHAT:
         return await edit_or_reply(
-            roaspl, "**Dasar ngentot, Lo gabisa Nyoba disini anjing!!!**"
+            event, "**Dasar ngentot, Lo gabisa Nyoba disini anjing!!!**"
         )
-    await roaspl.client.send_message,(
-        roaspl.chat_id,
+    await event.client.send_message,(
+        event.chat_id,
         "**EH MANUSIA HARAM YANG KERJAANYA REBAHAN, GAUSAH SOK SOK MENILAI HIDUP ORANG DEH LO BANGSAT, LO SENDIRI AJA GA MAMPU BUAT MAKAN GOBLOK KARNA LU GEMBEL KERJA LO PASTI JADI MANUSIA SILVER YANG KAN, NAH MAKANYA DARIPADA KEBANYAKAN NGURUSIN HIDUP ORAMG, MENDING LU CAT BADAN LU BURUAN ABISTU KERJA DAH NGEMIS NGEMIS KALO GAK MATUNG DI PINGGIR JALAN, YAHAHA KAYA BOCAH GA PUNYA KREATIFITAS ATAU MEMANG LO PUNYA KELAINAN DISABILITAS, YANG KELIATAN IDIOT TUU, PASTI JUGA LU LULUSAN SLB YAKAN BAGIAN ORANG ORANG YG KENA GANGGUAN JIWA ATAU PUNYA KELAINAN TU YKANN YAHAHAHAH WAHYOUEEEE🤪🤪**",
     )
-    await roaspl.delete()
+    await event.delete()
 
 
 @ram_cmd(pattern="roas2(?: |$)(.*)")
