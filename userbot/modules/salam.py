1e9c4f0b2745bc281e295ae168d7dd679d4236af
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-15 02:51:36 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -93,9 +93,9 @@ async def _(typew):
 @register(outgoing=True, pattern='^X(?: |$)(.*)')
 async def _(typew):
     if typew.chat_id in BLACKLIST_CHAT:
-         return await edit_or_reply(
-             typew, "**GABISA DISINI NGENTOT!!!**",
-         )
+        return await edit_or_reply(
+            typew, "**GABISA DISINI NGENTOT!!!**"
+        )
     await typew.client.send_message(
         typew.chat_id, "**GC SAMPAH, MEMBER CULIKAN MANA TYPINGAN SEPI BEGINI ISINYA FORWARD CHANNEL SAMA GCAST DOANG CUIHHHHH!!!!!!!!!!!**", reply_to=typew.reply_to_msg_id)
     await typew.delete()
