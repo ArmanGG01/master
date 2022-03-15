98e2e626dff8c732451b0cdd87f410a62c730729
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-15 16:21:28 +0700

    anj

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -710,9 +710,9 @@ with bot:
         @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid or event.query.user_id in DEVS and SUDO_USERS:
-                openlagi = custom.Button.inline("• Re-Open Menu •", data="reopen")
+                openlagi = custom.Button.inline("✴️ Open Menu ✴️", data="reopen")
                 await event.edit(
-                    "❌ **Help Mode Button Ditutup!** ❌", buttons=openlagi
+                    "✴️ **Help Mode Button Ditutup!** ✴️", buttons=openlagi
                 )
             else:
                 reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
