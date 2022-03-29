b97baa7eda538502f54bb8da1086a79e0bb74d47
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-30 00:51:49 +0700

    rewrite fgban

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -1,66 +1,75 @@
 # This is a troll indeed ffs *facepalm*
 # Ported from xtra-telegram by @heyworld
 import asyncio
+
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
-#from userbot.utils import admin_cmd
-from userbot.events import register
-from userbot import ALIVE_NAME, CMD_HELP, DEVS, bot
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, DEVS
+from userbot.utils import edit_or_reply, ram_cmd as cuy
+from userbot.events import register as bro
 
 
-@register(outgoing=True, pattern="^.fgban(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS, pattern=r"^\.cgbn(?: |$)")
+@cuy(pattern="fgban(?: |$)(.*)")
+@bro(pattern=r"^\.cgbn(?: |$)(.*)", sudo=True)
 async def gbun(event):
     if event.fwd_from:
         return
     gbunVar = event.text
     gbunVar = gbunVar[6:]
-    mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
-    no_reason = "No Reason Given "
-    await event.edit("**Summoning out the mighty gban hammer ☠️**")
+    me = await event.client.get_me()
+    mentions = f"**Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By** {me.first_name}\n"
+    await edit_or_reply(event, "`Mengaktifkan global banned....✅`")
     asyncio.sleep(3.5)
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
+    async for _ in event.client.iter_participants(
+        chat, filter=ChannelParticipantsAdmins
+    ):
         mentions += f""
     reply_message = None
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
-        replied_user = await event.client(GetFullUserRequest(reply_message.from_id))
+        replied_user = await event.client(GetFullUserRequest(reply_message.sender_id))
         firstname = replied_user.user.first_name
         usname = replied_user.user.username
-        idd = reply_message.from_id
+        idd = reply_message.sender_id
         # make meself invulnerable cuz why not xD
-        if idd == 1036951071:
-            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Heyworld](tg://user?id=1036951071) __to release your account__😏")
+        if idd in DEVS:
+            await reply_message.reply(
+                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Heyworld](tg://user?id=1036951071) __to release your account__😏"
+            )
         else:
-            jnl = ("`Warning!!`"
-                   "[{}](tg://user?id={})"
-                   f"` 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n\n"
-                   "**Name: ** __{}__\n"
-                   "**ID : ** `{}`\n"
-                   ).format(firstname, idd, firstname, idd)
+            jnl = (
+                f"**Global banned By** {me.first_name}\n\n"
+                "**Frist Name: ** {}\n"
+                "**User ID : ** `{}`\n"
+            ).format(firstname, idd)
             if usname is None:
                 jnl += "**Username: ** `Doesn't own a username!`\n"
             elif usname != "None":
                 jnl += "**Username** : @{}\n".format(usname)
             if len(gbunVar) > 0:
                 gbunm = "`{}`".format(gbunVar)
-                gbunr = "**Reason: **" + gbunm
+                gbunr = f"**Reason: **{gbunm}"
                 jnl += gbunr
             else:
+                no_reason = "**Reason: **`Kang Tipu VCS`"
                 jnl += no_reason
             await reply_message.reply(jnl)
     else:
         mention = (
-            f"Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By {DEFAULTUSER} \nReason: No Reason Given. ")
+            f"**Global Banned By** {me.first_name} \n**Reason:** `Anak Tolol` "
+        )
         await event.reply(mention)
     await event.delete()
 
-CMD_HELP.update({
-    "fakegban": "`.fgban`\
-    \nUsage: Type .fgban or Reply .fgban reason and see it yourself. "
-})
+
+CMD_HELP.update(
+    {
+        "fakegban": f"**Plugin : **`fakegban`\
+        \n\n  •  **Syntax :** `{cmd}fgban` <reply> <reason>\
+        \n  •  **Function : **Untuk melakukan aksi Fake global banned , just for fun\
+    "
+    }
+)
