7b77ea9008043d29c1da89b17ff67eb9833749ea
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-02 01:08:35 +0700

    reupdate arts.ly

--- userbot/modules/ramubot.py
+++ userbot/modules/ramubot.py
@@ -15,10 +15,10 @@ async def _(event):
     await xx.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
 
 
-@ram_cmd(pattern="misii(?: |$)(.*)")
+@ram_cmd(pattern="misi(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "`\n┻┳|―-∩`"
         "`\n┳┻|     ヽ`"
         "`\n┻┳|    ● |`"
@@ -26,14 +26,13 @@ async def _(event):
         "`\n┻┳|￣  )`"
         "`\n┳ﾐ(￣ ／`"
         "`\n┻┳T￣|`"
-        "\n**Punten**",
-    )
-
+        "\n**Punten**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @ram_cmd(pattern="pantau(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "`\n┻┳|―-∩`"
         "`\n┳┻|     ヽ`"
         "`\n┻┳|    ● |`"
@@ -41,14 +40,14 @@ async def _(event):
         "`\n┻┳|￣  )`"
         "`\n┳ﾐ(￣ ／`"
         "`\n┻┳T￣|`"
-        "\n**Masih Gua Pantau**",
-    )
+        "\n**Masih Gua Pantau**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 @ram_cmd(pattern="kodok(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⠄⠄⠄⠄⠄⣀⣀⣤⣶⣿⣿⣶⣶⣶⣤⣄⣠⣴⣶⣿⣶⣦⣄⠄\n"
         "⠄⣠⣴⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦\n"
         "⢠⠾⣋⣭⣄⡀⠄⠙⠻⣿⣿⡿⠛⠋⠉⠉⠉⠙⠛⠿⣿⣿⣿⣿\n"
@@ -63,14 +62,14 @@ async def _(event):
         "⠄⣿⡇⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⢀⣼⣿⣿⣿⣿⣿\n"
         "⠄⣿⡇⠄⠠⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿\n"
         "⠄⣿⠁⠄⠐⠛⠛⠛⠉⠉⠉⠉⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿\n"
-        "⠄⠻⣦⣀⣀⣀⣀⣀⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋\n",
-    )
+        "⠄⠻⣦⣀⣀⣀⣀⣀⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋\n", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 @ram_cmd(pattern="kodog(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⣿⣿⣿⡇⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
         "⣿⣿⣿⡇⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
         "⣿⣿⣿⡇⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
@@ -85,14 +84,14 @@ async def _(event):
         "⠑⠽⡻⢿⣮⣽⣷⣶⣯⣽⣳⠮⣽⣟⣲⠯⢭⣿⣛⡇⣿⣿⣿⣿\n"
         "⠄⠄⠈⠑⠊⠉⠟⣻⠿⣿⣿⣿⣷⣾⣭⣿⠷⠶⠂⣴⣿⣿⣿⣿\n"
         "⠄⠄⠄⠄⠄⠄⠄⠁⠙⠒⠙⠯⠍⠙⢉⣡⣶⣿⣿⣿⣿⣿⣿⣿\n"
-        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
-    )
+        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿\n", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 @ram_cmd(pattern="dtrump(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⣿⣿⣿⣿⣿⣿⡿⠿⠛⠋⠉⡉⣉⡛⣛⠿⣿⣿⣿⣿⣿⣿⣿⣿\n"
         "⣿⣿⣿⡿⠋⠁⠄⠄⠄⠄⠄⢀⣸⣿⣿⡿⠿⡯⢙⠿⣿⣿⣿⣿\n"
         "⣿⣿⡿⠄⠄⠄⠄⠄⡀⡀⠄⢀⣀⣉⣉⣉⠁⠐⣶⣶⣿⣿⣿⣿\n"
@@ -107,14 +106,14 @@ async def _(event):
         "⠄⠄⠄⠄⠄⠄⠈⢿⣿⠟⡋⠄⠄⠄⢣⠄⠄⠄⠄⠄⠈⠹⣿⣀\n"
         "⠄⠄⠄⠄⠄⠄⠄⠘⣷⣿⣿⣷⠄⠄⢺⣇⠄⠄⠄⠄⠄⠄⠸⣿\n"
         "⠄⠄⠄⠄⠄⠄⠄⠄⠹⣿⣿⡇⠄⠄⠸⣿⡄⠄⠈⠁⠄⠄⠄⣿\n"
-        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⡇⠄⠄⠄⢹⣧⠄⠄⠄⠄⠄⠄⠘\n",
-    )
+        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⡇⠄⠄⠄⢹⣧⠄⠄⠄⠄⠄⠄⠘\n", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 @ram_cmd(pattern="scina(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⣿⣿⣿⣿⠟⠋⢁⢁⢁⢁⢁⢁⢁⢁⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿\n"
         "⣿⣿⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⡀⠭⢿⣿⣿⣿⣿\n"
         "⣿⣿⣿⡟⠄⢀⣾⣿⣿⣿⣷⣶⣿⣷⣶⣶⡆⠄⠄⠄⣿⣿⣿⣿\n"
@@ -130,14 +129,14 @@ async def _(event):
         "⠄⠄⠄⠄⠄⠄⣿⡟⣷⠄⠹⣿⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄\n"
         "⠄⠄⠄⠄⠄⣸⣿⡷⡇⠄⣴⣾⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
         "⠄⠄⠄⠄⠄⣿⣿⠃⣦⣄⣿⣿⣿⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
-        "⠄⠄⠄⠄⢸⣿⠗⢈⡶⣷⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n",
-    )
+        "⠄⠄⠄⠄⢸⣿⠗⢈⡶⣷⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 @ram_cmd(pattern="wlcm(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\n"
         "───█▒▒░░░░░░░░░▒▒█───\n"
         "────█░░█░░░░░█░░█────\n"
@@ -147,14 +146,14 @@ async def _(event):
         "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█\n"
         "█░░║║║╠─║─║─║║║║║╠─░░█\n"
         "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█\n"
-        "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n",
+        "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n", reply_to=event.reply_to_msg_id,
     )
 
 
 @ram_cmd(pattern="gta(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⡀⠀⠀⠀⠀⠀⠀\n"
         "⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀\n"
         "⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⠟⠀⠀⠀⠀⠀⠀\n"
@@ -165,14 +164,14 @@ async def _(event):
         "⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣷\n"
         "⠀⠀⠘⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⢰⣾⣿⠏\n"
         "⠀⢠⣧⡔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠟⠁⠀\n"
-        "⠀⢸⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ Ah\nShit, here we go again.\n",
+        "⠀⢸⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ Ah\nShit, here we go again.\n", reply_to=event.reply_to_msg_id,
     )
-
+    await event.delete()
 
 @ram_cmd(pattern="sthink(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(
-        event,
+    await event.client.send_message(
+        event.chat_id,
         "⠀⠀⠀⠀⢀⣀⣀⣀\n"
         "⠀⠀⠀⠰⡿⠿⠛⠛⠻⠿⣷\n"
         "⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀\n"
@@ -187,17 +186,17 @@ async def _(event):
         "⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄\n"
         "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁\n"
         "⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁\n"
-        "⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟\n",
+        "⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟\n", reply_to=event.reply_to_msg_id,
     )
-
+    await event.delete()
 
 CMD_HELP.update(
     {
         "arts": f"**Plugin : **`arts`\
         \n\n  •  **Syntax :** `{cmd}kodok`\
-        \n  •  **Function : **arts Forg.\
+        \n  •  **Function : **arts Frog.\
         \n\n  •  **Syntax :** `{cmd}kodog`\
-        \n  •  **Function : **arts forg bundir.\
+        \n  •  **Function : **arts frog bundir.\
         \n\n  •  **Syntax :** `{cmd}dtrump`\
         \n  •  **Function : **arts donald Trump.\
         \n\n  •  **Syntax :** `{cmd}scina`\
@@ -208,7 +207,7 @@ CMD_HELP.update(
         \n  •  **Function : **arts si jhonson.\
         \n\n  •  **Syntax :** `{cmd}sthink`\
         \n  •  **Function : **arts berfikir\
-        \n\n  •  **Syntax :** `{cmd}punten` ; `{cmd}pantau`\
+        \n\n  •  **Syntax :** `{cmd}misi` ; `{cmd}pantau`\
         \n  •  **Function : **Arts Beruang kek lagi mantau.\
         \n\n  •  **Syntax :** `{cmd}sadboy`\
         \n  •  **Function : **ya sadboy coba aja.\
