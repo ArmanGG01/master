5a91ea72c28e4f4a2accb7de2bad14c53b7d4a70
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-07-19 03:35:38 +0700

    Update tools.py

--- rams/utils/tools.py
+++ rams/utils/tools.py
@@ -425,3 +425,5 @@ def download_lagu(url: str) -> str:
     info = ydl.extract_info(url, download=False)
     ydl.download([url])
     return os.path.join("downloads", f"{info['id']}.{info['ext']}")
+
+# null
