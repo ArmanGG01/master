4c8dd146906dc16ba339d4b9c8b70efb468fb48c
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-04 11:14:04 +0700

    update cukup.py

--- kars/modules/cukup.py
+++ kars/modules/cukup.py
@@ -1,10 +1,10 @@
 from asyncio import sleep
 from telethon.tl.types import ChatBannedRights
 from telethon.tl.functions.channels import EditBannedRequest
-from rams.events import register
-from rams import CMD_HELP
-from rams import CMD_HANDLER as cmd
-from rams.utils import ram_cmd
+from kars.events import register
+from kars import CMD_HELP
+from kars import CMD_HANDLER as cmd
+from kars.utils import ram_cmd
 # Aku nambah kredit sdikit
 # rama ganteng bgt kan?
 # Port By @VckyouuBitch From GeezProject
