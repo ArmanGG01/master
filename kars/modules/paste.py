61280ffe90fd71a1dcf856d19e150ce5341952d8
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-05 01:34:03 +0700

    Update paste.py

--- kars/modules/paste.py
+++ kars/modules/paste.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-"""rams module containing commands for interacting with dogbin(https://del.dog)"""
+"""kars module containing commands for interacting with dogbin(https://del.dog)"""
 
 import os
 
