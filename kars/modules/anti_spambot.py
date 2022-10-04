a9be7b8f250b4fe59595cfcb12c701f362438b1d
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-04 11:07:06 +0700

    update anti_spambot.py

--- kars/modules/anti_spambot.py
+++ kars/modules/anti_spambot.py
@@ -11,7 +11,7 @@ from requests import get
 from telethon.events import ChatAction
 from telethon.tl.types import ChannelParticipantsAdmins, Message
 
-from rams import ANTI_SPAMBOT, ANTI_SPAMBOT_SHOUT, BOTLOG_CHATID, CMD_HELP, bot
+from kars import ANTI_SPAMBOT, ANTI_SPAMBOT_SHOUT, BOTLOG_CHATID, CMD_HELP, bot
 
 
 @bot.on(ChatAction)
