68c1c95f6567919b35821445853801f704ef07b2
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-04 16:20:17 +0700

    update karssgban.py

--- kars/modules/karssgban.py
+++ kars/modules/karssgban.py
@@ -1,7 +1,7 @@
 from telethon.events import ChatAction
-from rams import DEVS, DEVG, owner, bot
-from rams.events import register
-from rams.utils import get_user_from_event, ram_cmd
+from kars import DEVS, DEVG, owner, bot
+from kars.events import register
+from kars.utils import get_user_from_event, ram_cmd
 
 # thanks: liualvinas & vckyoubitch
 
@@ -9,7 +9,7 @@ from rams.utils import get_user_from_event, ram_cmd
 async def handler(tele):
     if tele.user_joined or tele.user_added:
         try:
-            from rams.modules.sql_helper.gmute_sql import is_gmuted
+            from kars.modules.sql_helper.gmute_sql import is_gmuted
 
             guser = await tele.get_user()
             gmuted = is_gmuted(guser.id)
@@ -37,27 +37,27 @@ async def handler(tele):
 
 @ram_cmd(pattern="gbanb(?: |$)(.*)")
 @register(pattern=r"^\.cgbanb(?: |$)(.*)", sudo=True)
-async def gben(rams):
-    dc = rams
+async def gben(kars):
+    dc = kars
     sender = await dc.get_sender()
     me = await dc.client.get_me()
     if not sender.id == me.id:
         dark = await dc.reply("`Ingin Mengaktifkan Perintah Global Banned!`")
     else:
         dark = await dc.edit("`Memproses Global Banned Pengguna Ini!!`")
-    me = await rams.client.get_me()
+    me = await kars.client.get_me()
     await dark.edit(f"`Global Banned Akan Segera Aktif!!`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     f"@{me.username}" if me.username else my_mention
-    await rams.get_chat()
+    await kars.get_chat()
     a = b = 0
-    if rams.is_private:
-        user = rams.chat
-        reason = rams.pattern_match.group(1)
+    if kars.is_private:
+        user = kars.chat
+        reason = kars.pattern_match.group(1)
     else:
-        rams.chat.title
+        kars.chat.title
     try:
-        user, reason = await get_user_from_event(rams)
+        user, reason = await get_user_from_event(kars)
     except BaseException:
         pass
     try:
@@ -73,17 +73,17 @@ async def gben(rams):
                 f"MAAF BANGET JING 🤪, LO GABISA GBAN ADMIN @RAMSUPPORTT"
             )
         try:
-            from rams.modules.sql_helper.gmute_sql import gmute
+            from kars.modules.sql_helper.gmute_sql import gmute
         except BaseException:
             pass
         testuserbot = [
             d.entity.id
-            for d in await rams.client.get_dialogs()
+            for d in await kars.client.get_dialogs()
             if (d.is_group or d.is_channel)
         ]
         for i in testuserbot:
             try:
-                await rams.client.edit_permissions(i, user, view_messages=False)
+                await kars.client.edit_permissions(i, user, view_messages=False)
                 a += 1
                 await dark.edit(f"`Global Banned Aktif ✅`")
             except BaseException:
@@ -102,7 +102,7 @@ async def gben(rams):
 
 @ram_cmd(pattern=r"ungbanb(?: |$)(.*)")
 @register(pattern=r"^\.cungbanb(?: |$)(.*)", sudo=True)
-async def gunben(rams):
+async def gunben(kars):
     dc = rams
     sender = await dc.get_sender()
     me = await dc.client.get_me()
@@ -110,21 +110,21 @@ async def gunben(rams):
         dark = await dc.reply("`Membatalkan Perintah Global Banned Pengguna Ini`")
     else:
         dark = await dc.edit("`Membatalkan Perintah Global Banned`")
-    me = await rams.client.get_me()
+    me = await kars.client.get_me()
     await dark.edit(
         f"`Memulai Membatalkan Perintah Global Banned, Jangan Songong Lagi Ya!!!`"
     )
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     f"@{me.username}" if me.username else my_mention
-    await rams.get_chat()
+    await kars.get_chat()
     a = b = 0
     if rams.is_private:
-        user = rams.chat
-        reason = rams.pattern_match.group(1)
+        user = kars.chat
+        reason = kars.pattern_match.group(1)
     else:
-        rams.chat.title
+        kars.chat.title
     try:
-        user, reason = await get_user_from_event(rams)
+        user, reason = await get_user_from_event(kars)
     except BaseException:
         pass
     try:
@@ -138,17 +138,17 @@ async def gunben(rams):
                 "**MAAF MASZEH, DIA GA BISA DI BLACKLIST KARNA DIA DEVELOPER GUA NGENTOT LO!!**"
             )
         try:
-            from rams.modules.sql_helper.gmute_sql import ungmute
+            from kars.modules.sql_helper.gmute_sql import ungmute
         except BaseException:
             pass
         testuserbot = [
             d.entity.id
-            for d in await rams.client.get_dialogs()
+            for d in await kars.client.get_dialogs()
             if (d.is_group or d.is_channel)
         ]
         for i in testuserbot:
             try:
-                await rams.client.edit_permissions(i, user, send_messages=True)
+                await kars.client.edit_permissions(i, user, send_messages=True)
                 a += 1
                 await dark.edit(f"`Membatalkan Global Banned... Memproses... `")
             except BaseException:
