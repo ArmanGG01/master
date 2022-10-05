384213aa3b16e950c84ca89f255d76754f514761
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-05 17:48:37 +0700

    Update events.py

--- kars/events.py
+++ kars/events.py
@@ -194,7 +194,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**👑 KARMAN-UBOT LAPORAN EROR  👑"**\n\n"
+                    text = "**👑 KARMAN-UBOT LAPORAN EROR  👑**\n\n"
                     link = "[Group Support](https://t.me/obrolansuar)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
